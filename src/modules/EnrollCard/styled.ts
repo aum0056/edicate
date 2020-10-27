f6b6099f188bd4d2d681a22aff58612f2fb128c6
proudpdd <proud_panda@hotmail.com>
2020-10-28 01:31:03 +0700

    merge master

---
+++ src/modules/EnrollCard/styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const EnrollBox = styled.div`
+  border-radius: 0.25rem;
+  border: 0.5px solid #c4c4c4;
+  font-size: 20px;
+  display: flex;
+  align-items: center;
+  padding: 32px;
+  height: 60px;
+`;
